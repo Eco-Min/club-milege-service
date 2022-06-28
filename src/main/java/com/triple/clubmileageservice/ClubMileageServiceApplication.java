cc4d932fb2fd596a0e916c1a448d6c792aa4b7f8
Eco_Min <guldor10.b@gmail.com>
2022-06-29 00:12:14 +0900

    club-milege-service project 구성

---
+++ src/main/java/com/triple/clubmileageservice/ClubMileageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.triple.clubmileageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubMileageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubMileageServiceApplication.class, args);
+	}
+
+}
